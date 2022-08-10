4901a7aa6fe229177529c059457a497c3e6c0d3b
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2022-08-10 23:50:12 +0600

    Add files via upload

---
+++ Data Types/Intro.py
@@ -0,0 +1,5 @@
+string_type = "This is a String" #this is a string
+integer_type =677 #this is a interger
+
+print("For String Type Output : "+string_type)
+print("For interger Type Output : ",integer_type)
