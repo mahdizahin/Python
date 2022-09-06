85c7e178916adc10dabb410d1071a294adef0d87
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2022-09-07 04:29:24 +0600

    Add files via upload

---
+++ User Input/Company.py
@@ -0,0 +1,4 @@
+company_name = input ("Enter your company name : ")
+employe_in = input("Enter employee Numbers :")
+print("Your company name is : "+company_name)
+print("You have "+employe_in+" Numbers of Employee")
