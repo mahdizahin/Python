9c4152f5b58d5ddada5c7f90f01f84a35c810fa3
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2022-10-05 03:07:19 +0600

    Add files via upload

---
+++ If else/Park.py
@@ -0,0 +1,8 @@
+print("Welcome To Seydon park")
+age = int(input("Enter your age : "))
+if(age>10):
+    print("You can enter to the park")
+else:
+    print("Sorry,You can't enter in the park")
+
+
