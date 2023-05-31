962399d47c34ba595b6c4f4671d29e223295d6ed
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-05-31 19:01:30 +0600

    Add files via upload

---
+++ Swap/001.py
@@ -0,0 +1,9 @@
+Ex=16
+Fu=18
+
+Ex,Fu = Fu, Ex                   #swapping here
+
+
+print("Now Ex is : ",Ex)
+
+print("Now Fu is : ",Fu)
---
+++ Swap/002.py
@@ -0,0 +1,10 @@
+
+kuttarVoice = "Ghew Ghew"
+bilaierVoice = "Meaooo"
+
+#akhane amra swap korbo
+
+kuttarVoice,bilaierVoice= bilaierVoice,kuttarVoice
+
+print("So, kuttarVoice hocche : ",kuttarVoice)
+print("and bilaierVoice hocche :",bilaierVoice)
