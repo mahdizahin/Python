40413e503e22cb02e3aba904a5dcb0b5e1630761
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-05-31 18:16:52 +0600

    Add files via upload

---
+++ Sequence Type/001 sq.py
@@ -0,0 +1,22 @@
+list = ["apple","android","Windows","Linux","Raspberry"]
+
+print(list[2]+"\n")
+
+for loop in list:
+    print(loop)
+
+listx = input('Number Type kortei hobe\nEnter Number to list : ')
+
+
+for y in listx:
+    print("Tmr kopale ache "+y+" ta bhalo jinish")
+
+#tuple
+''' to hoise ki touple er jonno first bracate || parenthesis use korte hy
+'''
+
+
+AiNilamTouple = ("Amloki","JamThor","KathalOdin","kolaHimdoll")
+
+for go in AiNilamTouple:
+    print(go)
---
+++ Sequence Type/002 range.py
@@ -0,0 +1,6 @@
+
+run = range(78)
+print(run)
+
+for i in run:
+    print("I LOVE YOU",i)
