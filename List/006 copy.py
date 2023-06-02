6ff37482d6d35d6395e766847003c922e43774de
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-06-02 22:48:52 +0600

    Add files via upload

---
+++ List/006 copy.py
@@ -0,0 +1,22 @@
+
+
+liza=["ghar","dhoira","bathroom","er","moddhe","chubamu"]
+
+print(liza)
+print("\n")
+lcp=liza.copy()
+print(lcp)
+print("\n")
+lcp.sort()
+print(lcp)
+print("\n")
+lcp.sort(reverse=True)
+print(lcp)
+print("\n")
+for x in liza:
+    print(x)
+
+print("\n")
+
+for x in liza[2:4]:
+    print(x)
