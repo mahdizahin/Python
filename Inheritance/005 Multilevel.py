80d53f05c1b6201cd2fbd79682b3d0276ea87cf1
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-06-03 22:45:24 +0600

    Add files via upload

---
+++ Inheritance/005 Multilevel.py
@@ -0,0 +1,43 @@
+class x:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+
+    def displayX(self):
+        print(self.a,self.b)
+
+class y(x):
+    def __init__(self,a,b,c,d):
+        super().__init__(a,b)
+        self.c=c
+        self.d=d
+
+    def displayY(self):
+        super().displayX()
+        print(self.c,self.d)
+
+class z(y):
+    def __init__(self,a,b,c,d, e,f):
+        super().__init__(a,b,c,d)
+        self.e = e
+        self.f = f
+
+    def displayZ(self):
+        super().displayY()
+        print(self.e, self.f)
+
+class multipleCatcher (z):
+    def __init__(self,a,b,c,d,e,f,k):
+        super().__init__(a,b,c,d,e,f)
+        self.k=k
+
+    def display(self):
+
+        super().displayZ()
+        print(self.k)
+
+id = multipleCatcher(1,2,3,4,5,6,7)
+id.display()
+
+baby= multipleCatcher("Siam","ClassSeven","Single","s","s","s","K")
+baby.display()
