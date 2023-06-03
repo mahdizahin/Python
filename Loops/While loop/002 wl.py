9e441bb505cfc2375a3d2837ccdb1c85670b1206
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-06-03 13:44:16 +0600

    Add files via upload

---
+++ Loops/While loop/002 wl.py
@@ -0,0 +1,7 @@
+xions=[4,5,6,7,8,9]
+
+x=0
+
+while x<len(xions):
+    print(xions[x])
+    x=x+1
---
+++ Loops/While loop/003.py
@@ -0,0 +1,13 @@
+a=4
+v=3
+c=5
+d=0
+e=9
+
+arrat = [a,v,c,d,e]
+
+x=0
+
+while x<len(arrat):
+    print(arrat[x])
+    x=x+1
