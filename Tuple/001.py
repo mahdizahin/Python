f40e21fb8656538077060b6b901350fa7da9581c
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-06-03 07:41:22 +0600

    Add files via upload

---
+++ Tuple/001.py
@@ -0,0 +1,8 @@
+tpl=(1,2,3,4,5,6,7,86,9,)
+
+x=tpl[-2] #8
+
+print(x)
+
+print(tpl[3:5])
+print(tpl[6:5])
---
+++ Tuple/002.py
@@ -0,0 +1,12 @@
+#update
+
+tpl=(1,2,3,4,5)
+
+s=list(tpl)
+
+#print(type(s))
+s.append("zahin")
+
+final_rol= tuple(s)
+
+print(final_rol)
