8925a40dbcf114507e98719e45c0e506a7edd6cb
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-06-03 17:42:41 +0600

    Add files via upload

---
+++ Operators/001.py
@@ -0,0 +1,12 @@
+num1=75
+num2=25
+
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+mod=num1%num2
+exp=num1**num2 #power
+floor=num1//num2
+
+print("",add,"\n",sub,"\n",mul,"\n",div,"\n",mod,"\n",exp,"\n",floor)
